ec05fc9025f762be752c68e60963be9a6875a820
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-07-04 02:29:49 +0300

    dont splash screen + nevagate to home screen

--- lib/features/splash/presentation/manager/views/widgets/SplashViewBody.dart
+++ lib/features/splash/presentation/manager/views/widgets/SplashViewBody.dart
@@ -1,8 +1,36 @@
 import 'package:bookly/core/utilts/assets.dart';
+import 'package:bookly/features/home/data/presentation/views/view_model/views/home_view.dart';
+import 'package:bookly/features/splash/presentation/manager/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  @override
+  late AnimationController anim_controller;
+  late Animation<Offset> sliding_anim;
+  @override
+  void initState() {
+    super.initState();
+    init_sliding_anim();
+    Future.delayed(const Duration(seconds: 2), () {
+      navigate_to_home();
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    anim_controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -14,15 +42,27 @@ class SplashViewBody extends StatelessWidget {
           const SizedBox(
             height: 2,
           ),
-          const Text(
-            'read free books ',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          )
+          sliding_text(sliding_anim: sliding_anim)
         ],
       ),
     );
+  }
+
+  void navigate_to_home() {
+    Get.to(
+      () => const HomeView(),
+      transition: Transition.fadeIn,
+      duration: const Duration(milliseconds: 250),
+    );
+  }
+
+  void init_sliding_anim() {
+    anim_controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    super.initState();
+    sliding_anim =
+        Tween<Offset>(begin: const Offset(0, 3), end: const Offset(0, 0))
+            .animate(anim_controller);
+    anim_controller.forward();
   }
 }
