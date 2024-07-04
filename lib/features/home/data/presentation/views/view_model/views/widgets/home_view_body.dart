f4e55efee6e35669567e0d44a477113202a1ffdf
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-07-05 01:54:18 +0300

    create styles

--- lib/features/home/data/presentation/views/view_model/views/widgets/home_view_body.dart
+++ lib/features/home/data/presentation/views/view_model/views/widgets/home_view_body.dart
@@ -1,30 +1,33 @@
 import 'package:bookly/core/utilts/assets.dart';
 import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/Custom_Appbar.dart';
-import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/Custom_List_View_ITem.dart';
 import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/Featuredlistview.dart';
+import 'package:bookly/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:ui';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Custom_Appbar(),
-        Featuredlistview(),
+        const Custom_Appbar(),
+        const Featuredlistview(),
         Padding(
-          padding: EdgeInsets.only(left: 24, top: 47),
+          padding: const EdgeInsets.only(left: 24, top: 47),
           child: Text(
             'Best Seller',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
+            style: GoogleFonts.abel(
+              textStyle: Styles.textstyle18.copyWith(
+                fontSize: 26,
+              ),
             ),
           ),
         ),
-        Bestsellerlistviewitem()
+        const Bestsellerlistviewitem()
       ],
     );
   }
