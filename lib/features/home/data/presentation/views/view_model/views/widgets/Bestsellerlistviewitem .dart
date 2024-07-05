04a69ee352b8a9cfcd385c5388a09cdb45bf0343
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-07-05 21:03:34 +0300

    Best seller item done

---
+++ lib/features/home/data/presentation/views/view_model/views/widgets/Bestsellerlistviewitem .dart
@@ -0,0 +1,107 @@
+import 'package:bookly/core/utilts/assets.dart';
+import 'package:bookly/styles.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Bestsellerlistviewitem extends StatelessWidget {
+  const Bestsellerlistviewitem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 140,
+      child: Row(
+        children: [
+          const SizedBox(
+            width: 24,
+          ),
+          AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      assets_Data.first_book,
+                    ),
+                  )),
+            ),
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: const Text(
+                  'Harry Potter and the Goblet of Fire',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: Styles.textStyle20,
+                ),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              const Text(
+                'J.K. Rowling',
+                style: Styles.textStyle14,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Row(
+                children: [
+                  Text(
+                    '19.99 €',
+                    style: Styles.textStyle20,
+                  ),
+                  SizedBox(
+                    width: 39,
+                  ),
+                  BookRating(),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BookRating extends StatelessWidget {
+  const BookRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(
+          FontAwesomeIcons.solidStar,
+          color: Colors.yellow,
+          size: 20,
+        ),
+        const SizedBox(
+          width: 6,
+        ),
+        Text(
+          '4.8',
+          style: Styles.textStyle14.copyWith(
+              color: const Color(
+            0xffFFFFFF,
+          )),
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        const Text('(2390)'),
+      ],
+    );
+  }
+}
--- lib/features/home/data/presentation/views/view_model/views/widgets/home_view_body.dart
+++ lib/features/home/data/presentation/views/view_model/views/widgets/home_view_body.dart
@@ -1,8 +1,10 @@
 import 'package:bookly/core/utilts/assets.dart';
+import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/Bestsellerlistviewitem%20.dart';
 import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/Custom_Appbar.dart';
 import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/Featuredlistview.dart';
 import 'package:bookly/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:ui';
 
@@ -27,37 +29,12 @@ class HomeViewBody extends StatelessWidget {
             ),
           ),
         ),
-        const Bestsellerlistviewitem()
+        const SizedBox(
+          height: 20,
+        ),
+        const Bestsellerlistviewitem(),
       ],
     );
   }
 }
 
-class Bestsellerlistviewitem extends StatelessWidget {
-  const Bestsellerlistviewitem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 140,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.7 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.red,
-                  image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(
-                      assets_Data.first_book,
-                    ),
-                  )),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
