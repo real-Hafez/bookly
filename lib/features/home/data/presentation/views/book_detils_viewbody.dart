947f8c0b72f8c0fe46153afea38c65c58c45bdc7
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-07-07 01:13:25 +0300

    Book details view done

--- lib/features/home/data/presentation/views/book_detils_viewbody.dart
+++ lib/features/home/data/presentation/views/book_detils_viewbody.dart
@@ -1,7 +1,4 @@
-import 'package:bookly/core/widgets/cuatom_button.dart';
-import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/BookRating.dart';
-import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/books_Action.dart';
-import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/custom_book_image.dart';
+import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/book_dtls_sec.dart';
 import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/custom_app_bar_book_detils.dart';
 import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/similar_Books_list_view.dart';
 import 'package:bookly/styles.dart';
@@ -12,66 +9,45 @@ class book_detils_view_body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var witdh = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(
         horizontal: 30,
       ),
-      child: Column(
-        children: [
-          const custom_app_bar_book_detils(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: witdh * .2),
-            child: const custom_book_image(),
-          ),
-          const SizedBox(
-            height: 35,
-          ),
-          const Text(
-            'The Jungle Book',
-            style: Styles.textStyle30,
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          const Opacity(
-            opacity: 0.7,
-            child: Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle18,
+      child: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                const custom_app_bar_book_detils(),
+                const book_dtls_sec(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 51,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle14.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const similar_Books_list_view(),
+                const SizedBox(
+                  height: 40,
+                )
+              ],
             ),
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const Book_action(),
-          const SizedBox(
-            height: 51,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const similar_Books_list_view(),
-          const SizedBox(
-            height: 40,
           )
         ],
       ),
     );
   }
 }
+
---
+++ lib/features/home/data/presentation/views/view_model/views/widgets/book_dtls_sec.dart
@@ -0,0 +1,50 @@
+import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/BookRating.dart';
+import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/books_Action.dart';
+import 'package:bookly/features/home/data/presentation/views/view_model/views/widgets/custom_book_image.dart';
+import 'package:bookly/styles.dart';
+import 'package:flutter/material.dart';
+
+class book_dtls_sec extends StatelessWidget {
+  const book_dtls_sec({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var witdh = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: witdh * .2),
+          child: const custom_book_image(),
+        ),
+        const SizedBox(
+          height: 35,
+        ),
+        const Text(
+          'The Jungle Book',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        const Opacity(
+          opacity: 0.7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18,
+          ),
+        ),
+        const SizedBox(
+          height: 18,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(
+          height: 37,
+        ),
+        const Book_action(),
+      ],
+    );
+  }
+}
