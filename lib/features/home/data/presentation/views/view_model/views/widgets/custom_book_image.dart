d23d4643dcef2acf29d4198d314d9689ec16c900
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-07-08 17:43:50 +0300

    Cached network image

--- lib/features/home/data/presentation/views/view_model/views/widgets/custom_book_image.dart
+++ lib/features/home/data/presentation/views/view_model/views/widgets/custom_book_image.dart
@@ -1,4 +1,4 @@
-import 'package:bookly/core/utilts/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class custom_book_image extends StatelessWidget {
@@ -7,16 +7,17 @@ class custom_book_image extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      child: AspectRatio(
-        aspectRatio: 2.9 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: NetworkImage(image_url),
-              )),
-        ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: AspectRatio(
+            aspectRatio: 2.9 / 4,
+            child: CachedNetworkImage(
+              imageUrl: image_url,
+              fit: BoxFit.fill,
+              progressIndicatorBuilder: (context, url, downloadProgress) =>
+                  CircularProgressIndicator(value: downloadProgress.progress),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            )),
       ),
     );
   }
